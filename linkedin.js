9e6f61699ec0534bb2e4e056e6471489c2ff0675
Jambu <oedudealmeida@gmail.com>
2024-06-26 15:00:46 -0300

    first commit

---
+++ linkedin.js
@@ -0,0 +1,27 @@
+function removedAds() {
+
+    let spans = document.getElementsByTagName("span");
+
+    for (let i = 0; i < spans.length; ++i) {
+        if (spans[i].innerHTML === "Promoted") {
+            let card = spans[i].closest(".feed-shared-update-v2");
+
+            if (card === null) {
+                let j = 0;
+                card = spans[i];
+                while (j < 6) {
+                    card = card.parentNode;
+                    ++j;
+                }
+            }
+
+            card.setAttribute("style", "display:none");
+        }
+    }
+}
+
+removedAds();
+
+setInterval(function () {
+    removedAds();
+}, 100)
